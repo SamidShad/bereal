69dbf2f95ed10a5362e5dfbd96f22f798c02fbd1
samid <samidshadgithub@gmail.com>
2024-10-01 08:35:16 -0700

    vercel url added

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_url = "http://localhost:8002/api";
+const API_url = "https://berealapi.vercel.app/api";
 
 const token = localStorage.getItem("userToken")
   ? JSON.parse(localStorage.getItem("userToken"))?.token
